03548a1af79b5b9ad7730416bdc7d1d8c29ab0d1
Karan <kingkaran306@gmail.com>
2025-07-26 11:08:01 +0530

    two pointers

--- coding-interview-patterns-alex-xu/two-pointers/TripletSum.java
+++ coding-interview-patterns-alex-xu/two-pointers/TripletSum.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class TripletSum {
 
-    Set<List<Integer>> tripletSum(int[] arr) {
+    List<List<Integer>> tripletSum(int[] arr) {
         Set<List<Integer>> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
@@ -15,6 +15,40 @@ public class TripletSum {
                 }
             }
         }
-        return set;
+        return new ArrayList<>(set);
+    }
+
+
+    List<List<Integer>> tripletSumOptimized(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+            int left = i + 1, right = arr.length - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(arr[i], arr[left], arr[right]));
+                    while (left < right && arr[left] == arr[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && arr[right] == arr[right + 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+
+            }
+        }
+        return result;
     }
 }
