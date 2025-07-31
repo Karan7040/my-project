832b841f81a604a25117b1707d7086f533fe7c7c
Karan <kingkaran306@gmail.com>
2025-07-31 20:19:34 +0530

    trees

---
+++ coding-interview-patterns-alex-xu/trees/RightMostNodeOfBinaryTree.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class RightMostNodeOfBinaryTree {
+    public List<Integer> rightMostNodeOfBinaryTree(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+
+                if (i == size - 1) {
+                    result.add(node.val);
+                }
+            }
+            return result;
+
+        }
+    }
+}
